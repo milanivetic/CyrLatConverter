eae5755013ac1d327d2ee6d83e200f4cf99f6f08
milanivetic <50558388+milanivetic@users.noreply.github.com>
2019-05-13 22:36:47 +0200

    Add files via upload

--- src/cyrlatconverter_ignore_list_rs.js
+++ src/cyrlatconverter_ignore_list_rs.js
@@ -1,17 +1,32 @@
 /*
- * Cyrillic to Latin and vice versa converter - list of words to ignore
+ * Cyrillic to Latin and vice versa converter - ignore list
  *
- * ************************************************************************************
- * This software is free to use for personal, company internal or commercial purposes.
+ * NOTE: all keys in both arrays MUST be lowercase! 
+ * 
+ * /// CyrLatIgnore: ///
+ *     KEY: lowercase word to ignore, as in page it will ignore it regardless of is it lower, upper or mixed case.
+ *  VALUE: if value is set, word will be replaced with the value, if value empty, original word with original case will be preserved
+ *  
+ * /// CyrLatIgnore_doubleletters: ///
+ * 	  KEY: lowercase word to ignore double letters chaining. Ex. Nj => Њ by default, here you can specify words which ignore this behavior 
+ *  VALUE: always empty
  *
- * You may not resell this software, and attribution to the author must remain.
- * Backlink is desirable, you can show backlink to the:
- *   www.ivebe.com
- * or
- *   www.ivebe.com/blog/cyrillic-to-latin-and-latin-to-cyrillic-jquery-plugin.html
- * ************************************************************************************
- *
- * Copyright [2018] [Danijel Petrovic]
+ * /// CyrLatIgnore_doubleletters_base: ///
+ * 	  KEY: lowercase BASE word to ignore double letters chaining. Ex. Nj => Њ by default, here you can specify words which ignore this behavior. All words that contains base word will be ignored from double letters chaining.
+ *  VALUE: always empty
+ *  
+ * 
+ * ************************************************************************************ 
+ * This software is free to use for personal, company internal or commercial purposes. 
+ *                                                                                      
+ * You may not resell this software, and attribution to the author must remain.                  
+ * Backlink is desirable, you can show backlink to the:                                  
+ * www.ivebe.com                                                                        
+ * or                                                                                   
+ * www.ivebe.com/blog/cyrillic-to-latin-and-latin-to-cyrillic-jquery-plugin.html         
+ * ************************************************************************************ 
+ * 
+ * Copyright [2017] [Danijel Petrovic]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,14 +39,16 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @author Danijel Petrovic
- * @copyright Danijel Petrovic, www.ivebe.com, 2018
- * @version 1.0.0
+ * 
+ * 
+ * @author Danijel Petrovic 
+ * @copyright Danijel Petrovic, www.ivebe.com, 2017
+ * @version 0.6.2
  */
 	
-//ignore exact words, and if value is set, replace it with value
-let CyrLatIgnoreList = {
+	
+//ignore exact words, and if value set, replace it with value	
+var CyrLatIgnoreList = {
 	"plugin" : '',
 	"jquery" : 'jQuery',
 	"default" : '',
@@ -51,89 +68,103 @@ let CyrLatIgnoreList = {
 	"linux" : '',
 	"office" : '',	
  	"cookie" : '',
+ 	"online" : '',
  	"cyrlatconverter" : '',
+ 	"facebook": '',
+	"twitteru":'',
+ 	"cyrlatconvert" : '',
+ 	"http" : '', 
  	"skola@tsk.edu.rs" : '',
- 	"cisco" : '',
+	"cisco" : '',
     "ecdl" : '',
-    "oracle" : '',
     "I-" : '',
     "II-" : '',
     "III-" : '',
     "IV-" : ''
-    
 };
 
 //ignore EXACT words from double letters chaining into one
-let CyrLatIgnore_doubleLetters = [
-	'njemačku'
-];
+var CyrLatIgnore_doubleletters = {	
+	"OVDE-UNETI-TAČNU-REČ-ZA-IGNORISANJE-SPAJANJA-DVA-SLOVA-U-JEDNO" : '',
+	
+	
+	"gdje": '',
+	"ovdje": '',
+	"djed": '',
+	"odjeven": ''
+};
 
 //ignore all words BASED on words in the list from double letters chaining into one
-let CyrLatIgnore_doubleLettersBase = [
-	'anjon',
-	'adjektiv',
-	'adjunkt',
-	'budzašto',
-	'vanjezič',
-	'injekt',
-	'injekc',
-	'konjug',
-	'konjunk',
-	'lindzi',
-	'nadždrel',
-	'nadžet',
-	'nadžive',
-	'nadživ',
-	'nadžnje',
-	'nadžup',
-	'nadzemn',
-	'nadzida',
-	'nadzira',
-	'nadzire',
-	'nadziru',
-	'nadzor',
-	'nadjača',
-	'nadjaha',
-	'odžali',
-	'odžari',
-	'odžvaka',
-	'odžubor',
-	'odžive',
-	'odživlj',
-	'odzvanja',
-	'odzvoni',
-	'odziv',
-	'odjav',
-	'odjaha',
-	'odjaš',
-	'odjeb',
-	'odjedri',
-	'odjezdi',
-	'odjedanput',
-	'odjedared',
-	'odjednom',
-	'odjek',
-	'odjeci',
-	'odjur',
-	'podžanr',
-	'podzadat',
-	'podzakon',
-	'podzemlj',
-	'podzemn',
-	'podzida',
-	'podznak',
-	'podznaci',
-	'podjar',
-	'podjamči',
-	'podjezič',
-	'podjednak',
-	'predživot',
-	'predželuda',
-	'predzadnj',
-	'predznak',
-	'predznanj',
-	'predznaci',
-	'predjel',
-	'tanjug'
-];
-
+var CyrLatIgnore_doubleletters_base = {
+	anjon		: '',
+	adjektiv	: '',
+	adjunkt		: '',
+	budzašto	: '',
+	djet		: '',
+	djec		: '',
+	djel		: '',
+	djed		: '',
+	vanjezič	: '',
+	injekt		: '',
+	injekc		: '',
+	konjug		: '',
+	konjunk		: '',
+	lindzi		: '',
+	nadždrel	: '',
+	nadžet		: '',
+	nadžive		: '',
+	nadživ		: '',
+	nadžnje		: '',
+	nadžup		: '',
+	nadzemn		: '',
+	nadzida		: '',
+	nadzira		: '',
+	nadzire		: '',
+	nadziru		: '',
+	nadzor		: '',
+	nadjača		: '',
+	nadjaha		: '',
+	odžali		: '',
+	odžari		: '',
+	odžvaka		: '',
+	odžubor		: '',
+	odžive		: '',
+	odživ		: '',
+	odzvanja	: '',
+	odzvoni		: '',
+	odziv		: '',
+	odjav		: '',
+	odjaha		: '',
+	odjaš		: '',
+	odjeb		: '',
+	odjeć		: '',
+	odjedri		: '',
+	odjezdi		: '',
+	odjedanput	: '',
+	odjedared	: '',
+	odjednom	: '',
+	odjek		: '',
+	odjeci		: '',
+	odjur		: '',
+	podžanr		: '',
+	podzadat	: '',
+	podzakon	: '',
+	podzem		: '',
+	podzemn		: '',
+	podzida		: '',
+	podznak		: '',
+	podznaci	: '',
+	podjar		: '',
+	podjamči	: '',
+	podjezič	: '',
+	podjednak	: '',
+	predživot	: '',
+	predželuda	: '',
+	predzadnj	: '',
+	predznak	: '',
+	predznanj	: '',
+	predznaci	: '',
+	predjel		: '',
+	sudjel		: '',
+	tanjug		: ''
+};
